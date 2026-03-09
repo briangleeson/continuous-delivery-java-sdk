51f4feadc58745fbefe08b9b5bae2ad2f8c641f4
BRIAN GLEESON <Brian.Gleeson@ie.ibm.com>
2026-03-09 10:48:11 +0000

    chore: debug javadoc issue
    Signed-off-by: BRIAN GLEESON <Brian.Gleeson@ie.ibm.com>

--- build/publishJavadoc-gha.sh
+++ build/publishJavadoc-gha.sh
@@ -9,13 +9,14 @@ printf "\n>>>>> Publishing javadoc for release build: repo=%s tag=%s\n" ${GITHUB
 
 printf "\n>>>>> Cloning repository's gh-pages branch into directory 'gh-pages'\n"
 rm -fr ./gh-pages
-git config --global user.email "devxsdk@us.ibm.com"
-git config --global user.name "ibm-devx-sdk"
 git clone --branch=gh-pages https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO_SLUG}.git gh-pages
+# Configure the remote URL with the token for pushing
+git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPO_SLUG}.git
 
 printf "\n>>>>> Finished cloning...\n"
 
 pushd gh-pages
+
 
 # Create a new directory for this branch/tag and copy the javadocs there.
 printf "\n>>>>> Copying javadocs to new directory: docs/%s\n" ${GITHUB_TAG}
@@ -28,8 +29,12 @@ printf "\n>>>>> Generating gh-pages index.html...\n"
 
 printf "\n>>>>> Committing new javadoc...\n"
 git add -f .
+printf "\n>>>>> Added files...\n"
 git commit -m "docs: latest javadoc for ${GITHUB_TAG}"
+printf "\n>>>>> Committed changes...\n"
+git remote -v
 git push -f origin gh-pages
+printf "\n>>>>> Pushed changes...\n"
 
 popd
 
