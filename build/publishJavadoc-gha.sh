63a5de146fe311e8d3af2a8a24b9fd5e8034a840
BRIAN GLEESON <Brian.Gleeson@ie.ibm.com>
2026-03-09 15:05:18 +0000

    chore: clean up javadoc action
    Signed-off-by: BRIAN GLEESON <Brian.Gleeson@ie.ibm.com>

--- build/publishJavadoc-gha.sh
+++ build/publishJavadoc-gha.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 
-# set -e
+set -e
 
 # Required environment variables:
 # GITHUB_REPO_SLUG
 # GITHUB_TAG
 
-printf "\n>>>>> [DEBUG] current dir...\n"
-pwd
-ls -la
-printf "\n>>>>> [/DEBUG] current dir...\n"
-
 printf "\n>>>>> Publishing javadoc for release build: repo=%s tag=%s\n" ${GITHUB_REPO_SLUG} ${GITHUB_TAG}
-
-# printf "\n>>>>> Checking out gh-pages branch...\n"
-# git config --global user.email "github-actions[bot]@users.noreply.github.com"
-# git config --global user.name "github-actions[bot]"
-# # Checkout the gh-pages branch as a fresh copy of latest in main
-# git checkout -b gh-pages --no-guess
-# git status
-# printf "\n>>>>> Finished checking out...\n"
 
 # Open the "main" dir, containing the checked out main branch
 pushd main
 
 printf "\n>>>>> Generating Javadoc...\n"
 mvn -B versions:set -DnewVersion="${GITHUB_TAG}" -DgenerateBackupPoms=false
-mvn clean javadoc:aggregate --settings build/.github.settings.xml
+mvn -B -ntp clean javadoc:aggregate --settings build/.github.settings.xml
 printf "\n>>>>> Finished generating Javadoc...\n"
-
-printf "\n>>>>> [DEBUG] ls -la...\n"
-ls -la
-printf "\n>>>>> [/DEBUG] ls -la...\n"
 
 # Create a new directory for this branch/tag and copy the javadocs there.
 printf "\n>>>>> Copying javadocs to new directory: docs/%s\n" ${GITHUB_TAG}
